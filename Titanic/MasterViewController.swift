ac0ab79ffd3cec5af8428a9990cb97ae94eb203b
Travis Grammer <travisjgrammer@gmail.com>
2015-12-05 15:32:33 -0500

    Basic prepareForSegue function

--- Titanic/MasterViewController.swift
+++ Titanic/MasterViewController.swift
@@ -57,11 +57,14 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                //let object = objects[indexPath.row] as! NSDate
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                //controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                
+                controller.setRow(indexPath.row);
+                controller.assignPassengers(passengers);
             }
         }
     }
